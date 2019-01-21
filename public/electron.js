72e48246e8731e4d20b70e4146a22271a47a3c52
Paul Laffitte <paul.laffitte@epitech.eu>
2019-01-21 22:06:20 +0100

    production build for linux

--- public/electron.js
+++ public/electron.js
@@ -1,10 +1,12 @@
 const {app, BrowserWindow} = require('electron');
+// TODO const isDev = require('electron-is-dev'); 
 
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('http://localhost:3000');
+  mainWindow.loadFile(__dirname + '/../build/index.html');
+  // TODO isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
