8d0e1143b92b1e926fd83591fc5d4581b78855d9
Paul Laffitte <paul.laffitte@epitech.eu>
2019-03-05 12:04:19 +0100

    fix cards images display

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,35 +4,6 @@ import { getResourceByName } from '../state/selectors/deck';
 
 class Card extends Component {
 
-  state = {
-    width: 100,
-    height: 100
-  }
-
-  async componentDidMount() {
-    const refs = [this.refs.thumbnail, this.refs.background];
-
-    refs.forEach(ref => {
-      ref.onload = () => {
-        if (refs.every(ref => ref.complete))
-          this.renderCanvas();
-      }
-    });
-  }
-
-  async renderCanvas() {
-    const canvas = this.refs.canvas;
-    const [ thumbnail, background ] = await Promise.all([ this.refs.thumbnail, this.refs.background ]);
-    const { width, height } = background;
-    const ctx = canvas.getContext("2d");
-
-    if (this.state.width !== width || this.state.height !== height)
-      this.setState({ width, height });
-
-    ctx.drawImage(thumbnail, background.width / 2 - thumbnail.width / 2, 0);
-    ctx.drawImage(background, 0, 0);
-  }
-
   render() {
     return (
       <div className="card">
@@ -41,9 +12,8 @@ class Card extends Component {
         <div>HP: {this.props.hp}</div>
         <div>Attack: {this.props.attack}</div>
 
-        <canvas ref="canvas" width={this.state.width} height={this.state.height} />
-        <img ref="thumbnail" src={this.props.thumbnail} className="hidden" alt="canvas_source" />
-        <img ref="background" src={this.props.background} className="hidden" alt="canvas_source" />
+        <img ref="thumbnail" src={this.props.thumbnail} width={200} />
+        <img ref="background" src={this.props.background} width={200} />
       </div>
     );
   }
