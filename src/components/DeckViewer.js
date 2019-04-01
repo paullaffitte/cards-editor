d10f9875ed740f735a6928cc14da7892514f6b45
Paul Laffitte <paul.laffitte@epitech.eu>
2019-04-02 00:20:12 +0200

    deck viewer positions cards depending on the size of the 1st card

--- src/components/DeckViewer.js
+++ src/components/DeckViewer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import 'antd/dist/antd.css';
 import Card from './Card';
 import '../styles/DeckViewer.scss';
+import { getResourceByName } from '../state/selectors/deck';
 
 const makeSize = (width, height) => ({ width, height });
 const A4 = makeSize(297, 210);
@@ -13,11 +14,29 @@ const mmToPx = (mm, dpi) => inToPx(mmToIn(mm));
 
 class DeckViewer extends Component {
 
+  state = {
+    cardSize: makeSize(100, 100)
+  }
+
   positionToCss = (position) => ({
     position: 'absolute',
     top: `${position.y}px`,
     left: `${position.x}px`
   })
+
+  async componentDidMount() {
+    if (!this.props.cards.length)
+      return;
+
+    const onLoad = () => {
+      this.setState({ cardSize: makeSize(this.background.naturalWidth, this.background.naturalHeight) });
+    };
+
+    console.log(this.props.background);
+    this.background = new Image();
+    this.background.onload = onLoad;
+    this.background.src = this.props.background;
+  }
 
   renderCard = (card, { x, y, page }, spacing, cardSize) => {
     const position = {
@@ -35,7 +54,7 @@ class DeckViewer extends Component {
   renderCards = () => {
     const pageSize = makeSize(mmToPx(A4.width, this), mmToPx(A4.height));
     const spacing = 10;
-    const cardSize = makeSize(this.props.cardSize.width + spacing, this.props.cardSize.height + spacing);
+    const cardSize = makeSize(this.state.cardSize.width + spacing, this.state.cardSize.height + spacing);
     const A4px = makeSize(mmToPx(A4.width), mmToPx(A4.height));
 
     const maxBy = {
@@ -73,7 +92,9 @@ class DeckViewer extends Component {
 const mapStateToProps = state => ({
   cards: state.deck.current.cards,
   exportConfig: state.deck.current.exportConfig,
-  cardSize: makeSize(421, 601)
+  background: state.deck.current.cards.length
+    ? getResourceByName(state, state.deck.current.cards[0].background)
+    : null,
 });
 
 export default connect(mapStateToProps)(DeckViewer);
