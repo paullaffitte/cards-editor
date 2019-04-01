5139eaf357a911bfaa4f36c4f7da71a5b1564175
Paul Laffitte <paul.laffitte@epitech.eu>
2019-04-01 19:13:26 +0200

    window title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
@@ -6,9 +7,11 @@ import { Layout } from 'antd';
 import 'antd/dist/antd.css';
 import './App.scss';
 import reducers from './state/reducers/index';
+import { getCurrentDeck } from './state/selectors/deck';
 import DeckEditor from './components/DeckEditor';
 import DeckViewer from './components/DeckViewer';
 import DeckStorage from './services/DeckStorage';
+
 
 const store = createStore(
   reducers,
@@ -17,15 +20,34 @@ const store = createStore(
 
 DeckStorage.initFonts(store);
 
+const PageTitle = connect(state => ({
+  filename: getCurrentDeck(state).filename
+}))(class extends Component {
+
+  componentDidMount() {
+    document.originalTitle = document.title;
+  }
+
+  componentWillUpdate(nextProps) {
+    document.title = nextProps.filename
+      ? document.originalTitle + ' - ' + nextProps.filename
+      : document.originalTitle;
+  }
+
+  render = () => null;
+});
+
 class App extends Component {
 
   state = {
     exportMode: false
   }
 
+
   render() {
     return (
       <Provider store={store}>
+        <PageTitle />
         { !this.state.exportMode
           ? (
             <Layout className="app">
